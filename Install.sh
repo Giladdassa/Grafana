42812d614c42d2cd13bc8e34514b670b4e876881
Giladdassa <94293392+Giladdassa@users.noreply.github.com>
2021-11-15 16:08:01 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,24 +1,28 @@
-#!/bin/bash
+# #!/bin/bash
 
-#To install the latest Enterprise edition:
+# #To install the latest Enterprise edition:
 
-sudo apt-get install -y apt-transport-https
-sudo apt-get install -y software-properties-common wget
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+# sudo apt-get install -y apt-transport-https
+# sudo apt-get install -y software-properties-common wget
+# wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
-#Add this repository for stable releases:
+# #Add this repository for stable releases:
 
-echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana-enterprise -y
+# echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+# sudo apt-get update
+# sudo apt-get install grafana-enterprise -y
 
 
 
-#start automatically using systemd
+# #start automatically using systemd
 
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable grafana-server
+# sudo /bin/systemctl daemon-reload
+# sudo /bin/systemctl enable grafana-server
 
-#start grafana-server
+# #start grafana-server
 
-sudo /bin/systemctl start grafana-server
+# sudo /bin/systemctl start grafana-server
+
+
+sudo apt-get install -y apt-transport-https && sudo apt-get install -y software-properties-common wget && wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - && echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list && sudo apt-get update && sudo apt-get install grafana-enterprise -y && sudo /bin/systemctl daemon-reload && sudo /bin/systemctl enable grafana-server && sudo /bin/systemctl start grafana-server && grafana-cli plugins install grafana-azure-data-explorer-datasource && managed_identity_enabled = true
+ 
