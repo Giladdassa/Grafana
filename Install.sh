30c32839a2d7402654888350302b3253bd03fef4
Giladdassa <94293392+Giladdassa@users.noreply.github.com>
2021-11-16 18:22:32 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -24,5 +24,5 @@
 # sudo /bin/systemctl start grafana-server
 
 
-sudo apt-get install -y apt-transport-https && sudo apt-get install -y software-properties-common wget && wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - && echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list && sudo apt-get update && sudo apt-get install grafana-enterprise -y && sudo /bin/systemctl daemon-reload && sudo /bin/systemctl enable grafana-server && sudo /bin/systemctl start grafana-server && grafana-cli plugins install grafana-azure-data-explorer-datasource && managed_identity_enabled = true
+sudo apt-get install -y apt-transport-https && sudo apt-get install -y software-properties-common wget && sudo wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - && sudo echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list && sudo apt-get update && sudo apt-get install grafana-enterprise -y && sudo /bin/systemctl daemon-reload && sudo /bin/systemctl enable grafana-server && sudo /bin/systemctl && grafana-cli plugins install grafana-azure-data-explorer-datasource && start grafana-server
  
