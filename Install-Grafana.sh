bcd06ec21168f45d175a3344522866a7051e7c75
Giladdassa <94293392+Giladdassa@users.noreply.github.com>
2021-11-16 09:40:39 +0200

    Add files via upload

---
+++ Install-Grafana.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#To install the latest Enterprise edition:
+
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+#Add this repository for stable releases:
+
+echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install grafana-enterprise -y
+
+
+
+#start automatically using systemd
+
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable grafana-server
+
+#start grafana-server
+
+sudo /bin/systemctl start grafana-server
+
+########## Install Azure Data Explorer Plugin ############
+
+grafana-cli plugins install grafana-azure-data-explorer-datasource
+
+######### Enable Azure Managed Identity ##########
+
+managed_identity_enabled = true 
+
+
+
