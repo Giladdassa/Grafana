69a17b7f3ecd7e9fc816a72ae78734635f54de67
Giladdassa <94293392+Giladdassa@users.noreply.github.com>
2021-11-16 10:07:09 +0200

    Update Install-Grafana.sh

--- Install-Grafana.sh
+++ Install-Grafana.sh
@@ -1,35 +1,37 @@
-#!/bin/bash
+# #!/bin/bash
 
-#To install the latest Enterprise edition:
+# #To install the latest Enterprise edition:
 
-sudo apt-get install -y apt-transport-https
-sudo apt-get install -y software-properties-common wget
-sudo wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+# sudo -i
+#  apt-get install -y apt-transport-https
+#  apt-get install -y software-properties-common wget
+#  wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
-#Add this repository for stable releases:
+# #Add this repository for stable releases:
 
-echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana-enterprise -y
+# echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+#  apt-get update
+#  apt-get install grafana-enterprise -y
 
+# ########## Install Azure Data Explorer Plugin ############
 
-
-#start automatically using systemd
-
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable grafana-server
-
-#start grafana-server
-
-sudo /bin/systemctl start grafana-server
-
-########## Install Azure Data Explorer Plugin ############
-
-sudo grafana-cli plugins install grafana-azure-data-explorer-datasource
-
-######### Enable Azure Managed Identity ##########
-
-sudo managed_identity_enabled = true 
+#  grafana-cli plugins install grafana-azure-data-explorer-datasource
 
 
+# #start automatically using systemd
+
+#  /bin/systemctl daemon-reload
+#  /bin/systemctl enable grafana-server
+
+
+
+# ######### Enable Azure Managed Identity ##########
+
+#  managed_identity_enabled = true 
+
+# #start grafana-server
+
+#  /bin/systemctl start grafana-server
+
+sudu -i && apt-get install -y apt-transport-https && apt-get install -y software-properties-common wget && wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add - && echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list && apt-get update && apt-get install grafana-enterprise -y && grafana-cli plugins install grafana-azure-data-explorer-datasource && /bin/systemctl daemon-reload && /bin/systemctl enable grafana-server && /bin/systemctl start grafana-server
 
